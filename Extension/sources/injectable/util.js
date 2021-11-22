d7c5f1ea8574f6ee1f703ef8c842252fa69c21c5
Giles Miclotte <Giles.Miclotte@GMail.com>
2021-11-22 12:10:58 +0100

    Update pre-release version v1.6.0-9

--- Extension/sources/injectable/util.js
+++ Extension/sources/injectable/util.js
@@ -38,7 +38,7 @@
         MICSR.majorVersion = 1;
         MICSR.minorVersion = 6;
         MICSR.patchVersion = 0;
-        MICSR.preReleaseVersion = 8;
+        MICSR.preReleaseVersion = 9;
         MICSR.version = `v${MICSR.majorVersion}.${MICSR.minorVersion}.${MICSR.patchVersion}`;
         if (MICSR.preReleaseVersion !== undefined) {
             MICSR.version = `${MICSR.version}-${MICSR.preReleaseVersion}`;
