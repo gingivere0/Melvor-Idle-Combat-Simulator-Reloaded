f2d9deb210c933afbabfcd4d2224cf7fc137041e
Giles Miclotte <Giles.Miclotte@GMail.com>
2022-01-17 11:09:26 +0100

    Add missing pet Bone, #84

--- Extension/sources/injectable/App.js
+++ Extension/sources/injectable/App.js
@@ -165,6 +165,7 @@
                     22, 23, // slayer area pets
                     25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, // dungeon pets
                     45, // Mark
+                    46, // Bone
                 ];
 
                 // Forced equipment sorting
